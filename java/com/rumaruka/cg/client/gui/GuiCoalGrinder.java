c1eae81cca74048fed495fa04eef1ef5f40abe95
Rumaruka <rumarukakunnya@gmail.com>
2015-12-19 16:53:35 +0500

    Gui Grinder I Tier

--- java/com/rumaruka/cg/client/gui/GuiCoalGrinder.java
+++ java/com/rumaruka/cg/client/gui/GuiCoalGrinder.java
@@ -35,7 +35,6 @@ public class GuiCoalGrinder extends GuiContainer{
 	        int k = (this.width - this.xSize) / 2;
 	        int l = (this.height - this.ySize) / 2;
 	        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-
 	        if (this.teCG.isBurning())
 	        {
 	            int i1 = this.teCG.getBurnTimeRemainingScaled(13);
