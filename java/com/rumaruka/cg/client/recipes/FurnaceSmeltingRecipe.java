a171fe27ef49a4f6e65ad2d28f0b03430d670024
Rumaruka <rumarukakunnya@gmail.com>
2015-12-19 16:54:50 +0500

    Smelt Recipe

--- java/com/rumaruka/cg/client/recipes/FurnaceSmeltingRecipe.java
+++ java/com/rumaruka/cg/client/recipes/FurnaceSmeltingRecipe.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
+
 public class FurnaceSmeltingRecipe {
 	
 	public static void smeltingDust()
